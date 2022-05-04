38ac2bebe7d718293eb49f35bf924506c0e57aea
mijack <dr.mijack@gmail.com>
2022-05-05 02:08:36 +0300

    made it so the drawing area uses grid instead of flex box, alchanged layout

--- script.js
+++ script.js
@@ -1,80 +1,85 @@
+let gridItem = document.createElement("div");
+gridItem.setAttribute("class", "grid-item");
+let gridBox = document.querySelector("#grid-box");
+let colorPicker = document.querySelector("#color-picker");
+let rgbMode = document.querySelector("#rgb");
+let clearBtn = document.querySelector("#clear");
+let pixelSlider = document.querySelector("#density");
+let resolution = document.querySelector("#resolution");
 
-let gridItem =document.createElement('div');
-gridItem.setAttribute('class','grid-item');
-let gridBox = document.querySelector('#grid-box');
-let colorPicker= document.querySelector('#color-picker');
-let rgbMode= document.querySelector('#rgb');
-let clearBtn=document.querySelector('#clear');
-let pixelSlider= document.querySelector('#density');
-let resolution = document.querySelector('#resolution');
+let draw = false;
 
-
-
-colorPicker.addEventListener('change',(e)=>{
-    color=e.target.value;
-    colorMode=defaultMode;
-} );
-let oMode=document.querySelector('#opacity');
-
-rgbMode.addEventListener('click',()=> colorMode=RainbowMode);
-oMode.addEventListener('click',()=> {
-    colorMode=opacityMode;
-    color='rgba(0,0,0,0.1)';
+colorPicker.addEventListener("change", (e) => {
+  color = e.target.value;
+  colorMode = defaultMode;
 });
-let items = Number(pixelSlider.value)**2;
-let dimension = 500/Number(pixelSlider.value);
-let color= 'black';
-let randomNumber = () => Math.floor(Math.random()*255);
-let defaultMode = (e) =>{ 
-    e.target.style.opacity=1;
-    e.target.style.background=color;
-}
-let RainbowMode = (e) =>{
-    e.target.style.opacity=1;
-    e.target.style.background=`rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
-}
-let opacityMode = (e) =>{
-    if(e.target.style.background.match(/0.9/)){
-        return
-    }
-    opacity = e.target.style.background.match( /0.[0-9]/)
-     if(Number(opacity) <= 0.8){ 
-        e.target.style.background = `rgba(0,0,0,${Number(opacity)+0.1})`;  
-    }
-}
+let oMode = document.querySelector("#opacity");
+
+rgbMode.addEventListener("click", () => (colorMode = RainbowMode));
+oMode.addEventListener("click", () => {
+  colorMode = opacityMode;
+  color = "rgba(0,0,0,0.1)";
+});
+let items = Number(pixelSlider.value);
+let color = "black";
+let randomNumber = () => Math.floor(Math.random() * 255);
+let defaultMode = (e) => {
+  e.target.style.opacity = 1;
+  e.target.style.background = color;
+};
+let RainbowMode = (e) => {
+  e.target.style.opacity = 1;
+  e.target.style.background = `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
+};
+let opacityMode = (e) => {
+  if (e.target.style.background.match(/0.9/)) {
+    return;
+  }
+  opacity = e.target.style.background.match(/0.[0-9]/);
+  if (Number(opacity) <= 0.8) {
+    e.target.style.background = `rgba(0,0,0,${Number(opacity) + 0.1})`;
+  }
+};
 
 let colorMode = defaultMode;
 
 const createSketchPad = () => {
-    for(let i=0; i<items; i++){
-        gridItem.setAttribute('id','grid'+i);
-        gridItem.setAttribute('style',`width:${dimension}px;height:${dimension}px`)
-        gridBox.appendChild(gridItem.cloneNode(true));
-        document.querySelector('#grid'+i).addEventListener('mouseover',
-        (e) => colorMode(e));
-
-    }
+  gridBox.style.gridTemplate = `repeat(${items},1fr)/repeat(${items},1fr)`;
+  for (let i = 0; i < items ** 2; i++) {
+    gridItem.setAttribute("id", "grid" + i);
+    gridBox.appendChild(gridItem.cloneNode(true));
+    document.querySelector("#grid" + i).addEventListener("mouseover", (e) => {
+      // if (draw === false) return;
+      colorMode(e);
+    });
+  }
 };
 let remakeSketchPad = (boxSize) => {
-    let sketchPad = document.querySelector('#grid-box');
-    let childrenNum =sketchPad.childNodes.length
-    while(childrenNum > 0){
-        sketchPad.removeChild(sketchPad.childNodes[0]);
-        childrenNum--;
-    }
-    items =boxSize**2;
-    dimension=(500/boxSize).toFixed(3);
-    createSketchPad();
-}
+  let sketchPad = document.querySelector("#grid-box");
+  let childrenNum = sketchPad.childNodes.length;
+  while (childrenNum > 0) {
+    sketchPad.removeChild(sketchPad.childNodes[0]);
+    childrenNum--;
+  }
+  items = boxSize;
+  createSketchPad();
+};
 
-const clearSketchPad = () =>{
-    let div = document.querySelectorAll('.grid-item');
-    div.forEach(element => element.style.background='white')
-}
-clearBtn.addEventListener('click',clearSketchPad)
-pixelSlider.addEventListener('change',(e) => {
-    remakeSketchPad(Number(e.target.value));
-    resolution.innerText= `${e.target.value} x ${e.target.value}`
-})
-
+const clearSketchPad = () => {
+  let div = document.querySelectorAll(".grid-item");
+  div.forEach((element) => (element.style.background = "white"));
+};
+clearBtn.addEventListener("click", clearSketchPad);
+pixelSlider.addEventListener("change", (e) => {
+  remakeSketchPad(Number(e.target.value));
+  resolution.innerText = `${e.target.value} x ${e.target.value}`;
+});
+/** 
+gridBox.addEventListener("mousedown", () => {
+  draw = true;
+});
+gridBox.addEventListener("mouseup", () => {
+  draw = false;
+});
+*/
 createSketchPad();
